d1cceaad2a368068304183ddf666e9750b0eaef0
Luca Scrucca <luca.scrucca@unipg.it>
2020-12-16 11:20:02 +0000

    version 1.3.3

--- inst/doc/msir.R
+++ inst/doc/msir.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------
 library(knitr)
 opts_chunk$set(fig.align = "center", 
                out.width = "70%",
@@ -12,16 +12,15 @@ knit_hooks$set(par = function(before, options, envir)
        par(mar=c(4.1,4.1,1.1,1.1), mgp=c(3,1,0), tcl=-0.5)
 })
 
-library(rgl)
 # setupKnitr()
 # knit_hooks$set(rgl = hook_webgl)
 
 set.seed(1) # for exact reproducibility
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(msir)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 n <- 200
 p <- 5
 b <- as.matrix(c(1,-1,rep(0,p-2)))
@@ -33,7 +32,7 @@ plot(MSIR, type = "evalues")
 plot(MSIR, type = "coefficients", which = 1)
 plot(MSIR, type = "2Dplot")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 n <- 200
 p <- 5
 b <- as.matrix(c(1,-1,rep(0,p-2)))
@@ -45,7 +44,7 @@ plot(MSIR, type = "evalues")
 plot(MSIR, type = "coefficients", which = 1)
 plot(MSIR, type = "2Dplot")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 n <- 300
 p <- 5
 b1 <- c(1, 1, 1, rep(0, p-3))
@@ -61,56 +60,56 @@ plot(MSIR, which = 1:2)
 plot(MSIR, which = 1, type = "2Dplot", span = 0.7)
 plot(MSIR, which = 2, type = "2Dplot", span = 0.7)
 
-## ---- echo=1, fig.width = 4, fig.height = 4------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1, fig.width = 4, fig.height = 4----
 plot(MSIR, type = "spinplot")
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ---- echo=1, fig.width = 4, fig.height = 4------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1, fig.width = 4, fig.height = 4----
 plot(MSIR, type = "spinplot", span = 0.75)
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 x1 <- rnorm(100)
 x2 <- rnorm(100)
 y  <- 2*x1 + x2^2 + 0.5*rnorm(100)
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x1, y, x2)
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x1, y, x2, scaling="aaa")
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x1, y, x2, rem.lin.trend = "TRUE")
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x1, y, x2, fit.smooth = TRUE)
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x1, y, x2, fit.ols = TRUE)
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 x <- iris[,1:3]
 y <- iris[,5]
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x)
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x, markby = y)
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
-## ---- echo=1-------------------------------------------------------------
+## ---- eval=requireNamespace("rgl", quietly = TRUE), echo=1--------------------
 spinplot(x, markby = y, pch = c(0,3,1), 
          col.points = c("lightcyan", "yellow", "lightgreen"), 
          background = "black")
-rglwidget(width=500, height=450)
+rgl::rglwidget(width=500, height=450)
 
 ## ---- echo=-1-------------------------------------------------------------------------------------
 options(width=100)
